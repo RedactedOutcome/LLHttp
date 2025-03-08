b65a13945392bc1a9a882c3ead7bc5c4f3ff386a
The1WeirdDev <the1weirddev@gmail.com>
2025-03-07 20:37:28 -0600

    Update

---
+++ include/LLHttp/Core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#if __cplusplus < 201703L
+#error "LLHttp Requires a c++ version of atleast c++17"
+#endif
+
+#ifdef LLHTTP_USING_PCH
+
+#else
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#endif
---
+++ include/LLHttp/LLHttp.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Core.h"
+
+#include "HttpData.h"
+#include "HttpVerb.h"
+#include "Cookie.h"
+#include "Decoder.h"
+#include "HttpRequest.h"
+#include "HttpResponse.h"
+#incldue "WebSocketPacket.h"

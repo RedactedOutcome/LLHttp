831da6fe4a7325f46782ff120f74d349b24cd557
The1WeirdDev <the1weirddev@gmail.com>
2025-10-09 18:52:03 -0500

    Update

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -127,7 +127,7 @@ namespace LLHttp{
                     }
 
                     size_t startAt = m_At;
-                    std::cout << "Header start is " << m_Join.SubString(m_At, 15)<<std::endl;
+                    std::cout << "Header start is " << m_Join.SubString(m_At, 15).GetCStr()<<std::endl;
                     while(true){
                         char c = m_Join.Get(m_At);
                         if(c == ':')break;
@@ -151,7 +151,7 @@ namespace LLHttp{
                     }
 
                     size_t headerValueStart = ++m_At;
-                    std::cout << "Value Start is " << m_Join.SubString(m_At, 15)<<std::endl;
+                    std::cout << "Value Start is " << m_Join.SubString(m_At, 15).GetCStr() <<std::endl;
                     while(true){
                         char c = m_Join.Get(m_At);
                         std::cout<<"Char code " << (size_t)c<<std::endl;

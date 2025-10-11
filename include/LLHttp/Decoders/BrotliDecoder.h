e909a48a74a53a7d7b0a9a901293b230920ba11a
The1WeirdDev <the1weirddev@gmail.com>
2025-10-11 12:36:52 -0500

    Decode Brotli

--- include/LLHttp/Decoders/BrotliDecoder.h
+++ include/LLHttp/Decoders/BrotliDecoder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "pch.h"
+#include "HttpData.h"
 
 namespace LLHttp{
     class BrotliDecoder{
@@ -8,7 +9,59 @@ namespace LLHttp{
         BrotliDecoder()noexcept;
         ~BrotliDecoder()noexcept;
 
+        /// @brief Resets the entire state of the decoder
+        void Reset()noexcept;
+
+        /// @brief Incrementally Parses Brotli data.
+        /// @tparam Alloc 
+        /// @param data 
+        /// @param output 
+        /// @return 
+        template <typename Alloc=std::allocator<HBuffer>>
+        HttpEncodingErrorCode DecodeBrotli(HBuffer&& data, std::vector<HBuffer, Alloc> outputVector)noexcept{
+            m_Input.emplace_back(std::move(data));
+
+            constexpr int outputSize = 3200;
+            uint8_t output[outputSize];
+
+            for(auto it : m_Input){
+                const HBuffer& input = it.second;
+
+                uint8_t* inputData = reinterpret_cast<uint8_t*>(input.GetData());
+                size_t availableInput = input.GetSize();
+
+                size_t availableOut = outputSize;
+                uint8_t* outputBuffer = output;
+                size_t totalOut;
+
+                while(availableInput > 0){
+                    BrotliDecoderResult result = BrotliDecoderDecompressStream(m_State, &availableInput, &inputData, &availableOut, &outputBuffer, nullptr);
+
+                    if(result == BROTLI_DECODER_RESULT_ERROR)return HttpEncodingErrorCode::FailedDecodeBrotli;
+
+                    size_t produced = outputSize - availableOut;
+                    HBuffer outputBuffer = HBuffer(reinterpret_cast<char*>(output), produced, false, false).SubBuffer(0,-1);
+                    outputVector.emplace_back(std::move(outputBuffer));
+
+                    if(result == BROTLI_DECODER_RESULT_NEEDS_MORE_OUTPUT){
+                        outputBuffer = outputSize;
+                        availableOut = output;
+                        continue;
+                    }
+                    if(result == BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT){
+                        break;
+                    }
+                    if(result == BROTLI_DECODER_RESULT_SUCCESS){
+                        it = m_Input.erase(m_Input.begin(), it+1);
+                        break;
+                    }
+                }
+            }
+            m_Input.clear();
+            return HttpEncodingErrorCode::None;
+        }
     private:
         BrotliDecoderState* m_State = nullptr;
+        std::vector<HBuffer> m_Input;
     };
 }
--- src/Decoders/BrotliDecoder.cpp
+++ src/Decoders/BrotliDecoder.cpp
@@ -1,6 +1,16 @@
 #include "pch.h"
 
 namespace LLHttp{
-    BrotliDecoder::BrotliDecoder()noexcept{}
-    BrotliDecoder::~BrotliDecoder()noexcept{}
+    BrotliDecoder::BrotliDecoder()noexcept:
+    m_State(BrotliDecoderCreateInstance(nullptr, nullptr, nullptr)){
+
+    }
+    BrotliDecoder::~BrotliDecoder()noexcept{
+        BrotliDecoderDestroyInstance(m_State);
+    }
+
+    void BrotliDecoder::Reset()noexcept{
+        m_Input.clear();
+        BrotliDecoderReset(m_State);
+    }
 }

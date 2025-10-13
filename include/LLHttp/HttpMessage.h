bcb6fe7d5f048ffb1cb46d232813aa196d9d761e
The1WeirdDev <the1weirddev@gmail.com>
2025-10-13 15:16:24 -0500

    Fixed cast

--- include/LLHttp/HttpMessage.h
+++ include/LLHttp/HttpMessage.h
@@ -92,7 +92,7 @@ namespace LLHttp{
         const HBufferJoin& GetJoin()const noexcept{return m_Join;}
         size_t GetParseAt()const noexcept{return m_At;}
         size_t GetRemaining()const noexcept{return m_Remaining;}
-        HBufferJoin& GetJoinRef() noexcept{return m_Join;}
+        HBufferJoin& GetJoinRef() noexcept{return (HBufferJoin&)m_Join;}
     protected:
         HttpVersion m_Version = HttpVersion::HTTP1_1;
         std::vector<HBuffer, BodyAlloc> m_Body;

3f4b5a4de18aa965b0d27ecdc689218df299c024
The1WeirdDev <the1weirddev@gmail.com>
2025-11-22 09:30:20 -0600

    Update

--- include/LLHttp/HttpResponse.h
+++ include/LLHttp/HttpResponse.h
@@ -174,7 +174,7 @@ namespace LLHttp{
             const it& = m_Headers.find(HBuffer(std::forward<Args>(args)...));
             if(it == m_Headers.end())
                 throw std::out_of_range("FindHeader looked for non existant header");
-            return *it;
+            return it.second;
         }
     public:
         Cookie& GetCookie(const char* name) noexcept;
